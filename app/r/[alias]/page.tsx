d4f8cacf5ddbd811f66926faaa1a3396e54a931e
Evren Yaman <yamane@bu.edu>
2025-04-18 18:01:47 -0400

    deployment changes

--- app/r/[alias]/page.tsx
+++ app/r/[alias]/page.tsx
@@ -1,14 +1,16 @@
 import { redirect } from "next/navigation";
 import { getUrlByAlias } from "@/lib/getUrlByAlias";
 
-export default async function RedirectPage(props: { params: { alias: string } }) {
-    const alias = props.params.alias;
-    const url = await getUrlByAlias(alias);
-  
-    if (!url) {
-      redirect("/");
-    }
-  
-    redirect(url);
+export default async function RedirectPage({
+  params,
+}: {
+  params: { alias: string };
+}) {
+  const url = await getUrlByAlias(params.alias);
+
+  if (!url) {
+    redirect("/");
   }
-  
+
+  redirect(url);
+}
